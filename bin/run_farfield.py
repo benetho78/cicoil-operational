76d204680b8b5ffc797c0451087e94ff4cd32341
Juan Carlos Cerda Chacon <jcerda@cicese.mx>
2022-05-24 12:00:02 -0700

    Version 1.0 working in Kanik

---
+++ bin/run_farfield.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 08:39:37 2022
+
+@author: jcerda
+"""
+
+import os
+import sys
+from datetime import datetime, timedelta
+import yaml
+import argparse
+import datetime as dt
+import numpy as np
+
+from opendrift.readers import reader_netCDF_CF_generic
+from opendrift.models.tamoc_plume import Plume
+from opendrift.readers import reader_ROMS_native
+from opendrift.readers import reader_NEMO_native
+from opendrift.models.ciceseoil import OpenCiceseOil
+# from opendrift.readers import reader_basemap_landmask
+# from opendrift.models.openplume3D import OpenPlume3D
+
+# import netCDF4 as nc
+# import xarray as xr
+# from siphon.catalog import TDSCatalog
+# from subprocess import Popen
+
+def getSafeOutputFilename(proposedFilename, fextension, count=0):
+    if os.path.exists(proposedFilename + '.' + fextension):
+        if proposedFilename.split('_')[-1].isnumeric():
+            count = int(proposedFilename.split('_')[-1])
+            proposedFilename = '_'.join(proposedFilename.split('_')[0:-1])
+        nproposedFilename = proposedFilename + '_' + str(count+1)
+        return getSafeOutputFilename(nproposedFilename, fextension, count+1)
+    else:
+        return proposedFilename + '.' + fextension
+
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser(description='Run far field model OPENDRIFT', prog='run_farfield')
+  parser.add_argument('--subset', '-s', action='store', dest='PtoParam', help='yaml file with the information about point.')
+  parser.add_argument('--commands', '-c', action='store_true', dest='show_commands', help='Just show the commands to run')
+
+  # args = parser.parse_args('--config-file', "../Pto_Config.yaml")
+  args = parser.parse_args()
+  # TODO Add verbose mode
+  if args.PtoParam:
+      with open(args.PtoParam, 'r') as stream:
+          try: 
+              PtoParam = yaml.safe_load(stream)
+          except:
+              print ('Something went wrong reading ' + args.subsetconfig)            
+
+  # IF date is NOT given, run the present day
+  if PtoParam['sim']['StartTime'] == None:
+      # starttime = (dt.datetime.today() - dt.timedelta(days=1)).strftime("%Y%m%d")
+      # endtime = (dt.datetime.today() + dt.timedelta(days=PtoParam['cicoil']['sim_len'])).strftime("%Y%m%d")
+      starttime = (dt.datetime.today().replace(microsecond=0, second=0, minute=0, hour=0) - dt.timedelta(days=1))
+      endtime = (dt.datetime.today().replace(microsecond=0, second=0, minute=0, hour=0) + dt.timedelta(days=PtoParam['cicoil']['sim_len']))
+  else:
+      # smonth = int(PtoParam['sim']['Smonth'] + 1)
+      starttime = dt.datetime(int(PtoParam['sim']['StartTime'][0:4]), int(PtoParam['sim']['StartTime'][4:6]), int(PtoParam['sim']['StartTime'][6:8]),
+                              PtoParam['sim']['Shour'], 0)
+      # emonth = int(PtoParam['sim']['Emonth'] + 1)
+      endtime = dt.datetime(int(PtoParam['sim']['EndTime'][0:4]), int(PtoParam['sim']['EndTime'][4:6]), int(PtoParam['sim']['EndTime'][6:8]),
+                            PtoParam['sim']['Ehour'], 0)
+      
+  txtTime = starttime.strftime("%Y%m%d-%H")  # str(starttime)[0:10]
+  # Read or define wind_factor
+  if PtoParam['cicoil']['wind_factor'] != None:
+      wind_factor = float(PtoParam['cicoil']['wind_factor'])
+  else: wind_factor = 0.035
+  
+  # Select Simulation location and results output file
+  output_path = os.path.join(PtoParam['outdir'],PtoParam['point']['name'],txtTime[0:8])
+  
+  # TAMOC output dir
+  txtTime = starttime.strftime("%Y%m%d-%H")  # str(starttime)[0:10]
+  output_tamoc = os.path.join(PtoParam['outdir'],PtoParam['point']['name'],txtTime[0:8],'TAMOC_output_files/')
+  
+  sim_name = PtoParam['sim_name']
+  sim_duration = timedelta(days=int(PtoParam['cicoil']['sim_len']))
+  particles_number = float(PtoParam['cicoil']['N_parti'])
+  step_time = float(PtoParam['cicoil']['step_time'])           # hours
+  output_step_time = float(PtoParam['cicoil']['repo_time'])    # hours
+  release_points = np.int(1)
+
+  seed_duration = endtime - starttime
+  leak_days = seed_duration.days
+  leak_remainder = endtime - (starttime + timedelta(days=leak_days))
+  seed_days_exact = seed_duration.total_seconds() / 86400.     # seconds per day
+  particles_per_day = int(np.ceil(particles_number / seed_days_exact))
+  remaining_particles = int(particles_per_day * divmod(seed_days_exact, leak_days)[1])
+  if remaining_particles > 0: leak_days += 1
+
+  lon = float(PtoParam['point']['lon'])
+  lat = float(PtoParam['point']['lat'])
+  oil_name = PtoParam['oil']['name']
+  flow_rate = float(PtoParam['spill']['flow_rate'])       # kg/s
+  verticle_angle = - np.pi / 180 * int(PtoParam['spill']['vertical_angle']) # convert degrees to radians
+  horiz_angle = np.pi / 180 * int(PtoParam['spill']['horizont_angle'])      # convert degrees to radians
+  GOR = float(PtoParam['spill']['GOR'])                   # scf/bbl
+  release_depth = float(PtoParam['spill']['depth'])       # meters
+  jet_diameter = float(PtoParam['spill']['jet_diam'])     # meters
+  fluid_temp = 273.15 + float(PtoParam['spill']['temp'])  # convert Celsius to Kelvin
+  bins = int(PtoParam['spill']['bins'])
+  
+  # Create and configure OpenCiceseOil
+  weathering=PtoParam['model']['weathering']
+  fartype=PtoParam['model']['fartype']
+
+  o = OpenCiceseOil(loglevel=20, weathering_model=weathering)
+  if fartype == '2D Sim.':
+      o.disable_vertical_motion()
+  else:
+      o.set_config('processes:dispersion', False)
+
+  o.set_config('drift:advection_scheme',PtoParam['model']['advection_scheme'])      # Added by "abgarcia"
+  o._set_config_default('drift:current_uncertainty', 0.0)
+  o._set_config_default('drift:wind_uncertainty', 0.0)
+
+  # READERS
+  if PtoParam['input']['curr'] == 'fnmoc':
+      reader_current = reader_netCDF_CF_generic.Reader(filename=PtoParam['datadir'] + 'fnmoc-amseas/' + txtTime[0:8] + '/fnmoc-amseas-forecast-GoM-' + txtTime[0:8] + '-time*.nc', name='amseas_forecast')
+  elif PtoParam['input']['curr'] == 'hycom':
+      reader_current = reader_netCDF_CF_generic.Reader(filename=PtoParam['datadir'] + 'hycom/HYCOM-forecast-GoM-' + txtTime[0:8] + '.nc', name='hycom_forecast')
+  if PtoParam['input']['wind'] == 'gfs':
+      reader_winds = reader_netCDF_CF_generic.Reader(filename=PtoParam['datadir'] + 'gfs-winds/' + 'gfs-winds-forecast-GoM-' + txtTime[0:8] + '.nc', name='gfs_forecast')
+  # print(reader_winds)
+  #print(reader_winds)
+  #print(reader_current)
+
+  # o.add_reader([reader_basemap, reader_globcurrent, reader_oceanwind])
+  o.add_reader([reader_current, reader_winds])
+  o.set_oiltype(oil_name)
+
+  # load TAMOC files
+  tamoc_file= ''.join((output_tamoc, PtoParam['sim_name'][0:-3], '-tamoc_', txtTime, '_', PtoParam['point']['name'],'_',str(release_depth.as_integer_ratio()[0]),'m'))
+  
+
+  # Seed to Far-field and run
+  spillets = particles_per_day
+  for day in range(leak_days):
+      tamoc_plume = Plume(plume_file=tamoc_file + '_plume.nc')
+      tamoc_plume.get_particle_properties(particles_file=tamoc_file + '_particles.nc')
+      
+      stoptime = starttime + timedelta(days=day + 1)
+      if day==leak_days - 1 and remaining_particles > 0:
+          spillets = remaining_particles
+          stoptime = starttime + timedelta(days=day) + leak_remainder
+          
+      o.seed_plume_elements(lon, lat, tamoc_plume, starttime+ timedelta(days=day), stoptime,
+                               number=spillets, z_uncertainty=10, wind_drift_factor=wind_factor,
+                               oiltype=oil_name)
+
+  cicoil_path = os.path.join(output_path,'CICOIL_output_files/')
+  try:
+      os.makedirs(cicoil_path) 
+  except OSError as error:
+      print(error)
+
+  output_file = cicoil_path + PtoParam['point']['name'] + '_' + sim_name + '_' + starttime.strftime("%Y%m%d-%H") + '_' + endtime.strftime("%Y%m%d-%H") + '.nc'
+  print(o)
+  o.run(duration=sim_duration,
+            time_step=timedelta(hours=step_time),
+            time_step_output=timedelta(hours=output_step_time),
+            outfile=output_file)
+  print(o)
+  
+  # Post processing
+  figs_path = os.path.join(output_path,'Figures/')
+  try:
+      os.makedirs(figs_path) 
+  except OSError as error:
+      print(error)
+
+  postp_file=''.join((figs_path,PtoParam['point']['name'], '_', sim_name,'_',starttime.strftime("%Y%m%d-%H")))
+
+  # Maps
+  o.plot(filename=postp_file +'_trajectories_zoom.png')
+  o.plot(filename=postp_file +'_trajectories.png', corners=PtoParam['point']['corners'])
+  o.plot_oil_budget(filename=postp_file + '_budget-01.png')                     # Added by "abgarcia"
+  o.plot_oil_budget(show_density_viscosity=True, show_wind_and_current=True,filename=postp_file + '_budget-02.png',) # Added by "abgarcia"
+  
+  # Animations
+  o.animation(background=['x_sea_water_velocity', 'y_sea_water_velocity'],
+              colorbar=True, fps=10, filename=postp_file + '_zoom.gif',vmin=0, vmax=2.0)
+  o.animation(background=['x_sea_water_velocity', 'y_sea_water_velocity'],
+              colorbar=True, fps=10, filename=postp_file + '.gif', corners=PtoParam['point']['corners'], vmin=0, vmax=2.0)
+
+  o.animation(color='viscosity', fsp=10, filename=postp_file + '_viscosity.gif') # Added by "abgarcia"
+  o.animate_vertical_distribution(filename=postp_file + '_vertical.gif')
+  o.animation_profile(fps=10, filename=postp_file + '_profile.gif')
---
+++ bin/run_nearfield.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 09:59:53 2022
+
+@author: jcerda
+Tomado: Koztaz GUI
+"""
+import os
+from opendrift.readers import reader_netCDF_CF_generic
+from opendrift.models.ciceseoil import OpenCiceseOil
+import opendrift.models.vertical_profile as vp
+import opendrift.models.run_tamoc as tamoc
+import numpy as np
+import yaml
+import argparse
+import datetime as dt
+
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser(description='Run near field model TAMOC', prog='run_nearfield')
+  parser.add_argument('--subset', '-s', action='store', dest='PtoParam', help='yaml file with the information about point.')
+  parser.add_argument('--commands', '-c', action='store_true', dest='show_commands', help='Just show the commands to run')
+
+  # args = parser.parse_args('--config-file', "../Pto_Config.yaml")
+  args = parser.parse_args()
+  # TODO Add verbose mode
+  if args.PtoParam:
+      with open(args.PtoParam, 'r') as stream:
+          try: 
+              PtoParam = yaml.safe_load(stream)
+          except:
+              print ('Something went wrong reading ' + args.subsetconfig)
+
+  # IF date is NOT given, run the present day
+  if PtoParam['sim']['StartTime'] == None:
+      # starttime = (dt.datetime.today() - dt.timedelta(days=1))
+      starttime = (dt.datetime.today().replace(microsecond=0, second=0, minute=0, hour=0) - dt.timedelta(days=1))
+  else:
+      starttime = dt.datetime(int(PtoParam['sim']['StartTime'][0:4]), int(PtoParam['sim']['StartTime'][4:6]), int(PtoParam['sim']['StartTime'][6:8]),
+                              PtoParam['sim']['Shour'], 0)
+
+  # output paths
+  txtTime = starttime.strftime("%Y%m%d-%H")  # str(starttime)[0:10]
+  output_path = os.path.join(PtoParam['outdir'],PtoParam['point']['name'],txtTime[0:8],'TAMOC_output_files/')
+  try:
+      os.makedirs(output_path) 
+  except OSError as error:
+      print(error)
+  
+  # load Point info
+  lon = float(PtoParam['point']['lon'])
+  lat = float(PtoParam['point']['lat'])
+  
+  # load oil properties
+  oil_origin = PtoParam['oil']['origin']                  # Ex: "GULF OF MEXICO, USA"
+  oil_name = PtoParam['oil']['name']                      # Ex: "GENERIC BUNKER C"
+  flow_rate = float(PtoParam['spill']['flow_rate'])       # Ex: 40.0 kg/s
+  verticle_angle = - np.pi / 180 * float(PtoParam['spill']['vertical_angle'])  # Convert degrees to radians
+  horiz_angle = np.pi / 180 * float(PtoParam['spill']['horizont_angle'])       # Convert degrees to radians
+  GOR = float(PtoParam['spill']['GOR'])                   # Ex: 1500.0 scf/bbl
+  release_depth = float(PtoParam['spill']['depth'])       # Ex: 400.0 m
+  jet_diameter = float(PtoParam['spill']['jet_diam'])     # Ex: 0.2 m
+  fluid_temp = 273.15 + float(PtoParam['spill']['temp'])  # Ex: 65.0 convert Celsius to Kelvin
+  bins = int(PtoParam['spill']['bins'])                   # Ex: 12
+  
+   # Create live-oil composition
+  weathering=PtoParam['model']['weathering']
+  llevel=PtoParam['model']['log_level']
+  o = OpenCiceseOil(loglevel=llevel, weathering_model=weathering)
+  o.set_oiltype(oiltype=oil_name)
+  live_comp, composition, chemdata, chemunits = o.fluid_properties.get_live_composition(GOR=GOR)
+
+  # Create vertical profiles
+  tamoc_time = starttime
+  if PtoParam['input']['curr'] == 'fnmoc':
+      ncDataIn= ''.join([PtoParam['datadir'],'fnmoc-amseas/',txtTime[0:8],'/fnmoc-amseas-forecast-GoM-', txtTime[0:8], '-time',txtTime[9:],'.nc'])
+  elif PtoParam['input']['curr'] == 'hycom':
+      ncDataIn= ''.join([PtoParam['datadir'],'hycom/HYCOM-forecast-GoM-',txtTime[0:8],'.nc'])
+
+  profile_name = ''.join((output_path, PtoParam['sim_name'][0:-3],'-v_profile_', txtTime, '_', PtoParam['point']['name'], '.nc'))
+
+  readers=reader_netCDF_CF_generic.Reader(ncDataIn)
+  vp.create_profile(profile_name, readers, lon, lat, tamoc_time)
+
+  # run near-field model
+  output_name = ''.join((output_path,PtoParam['sim_name'][0:-3],'-tamoc_', txtTime, '_', PtoParam['point']['name'],'_',str(release_depth.as_integer_ratio()[0]),'m'))
+  tamoc.run_tamoc(live_comp, composition, chemdata, chemunits, output_name, profile_name,
+                                  total_flow=flow_rate, z0=release_depth, D=jet_diameter, Tj=fluid_temp,
+                                  phi_0=verticle_angle, theta_0=horiz_angle, bins=bins)
+  
+  print('\n\n nearfield run finished!!! \n\n')
